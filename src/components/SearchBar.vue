e933faf9f53dc40c243919e558a53b075659a78d
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-12-19 09:36:06 -0700

    TopNav scaffold

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-nav-form>
+        <b-form-input class="mr-sm-2" type="text" placeholder="Search"></b-form-input>
+        <b-button variant="secondary" type="submit">Search</b-button>
+    </b-nav-form>
+</template>
+
+<script>
+import bNavForm from 'bootstrap-vue/es/components/nav/nav-form'
+import bFormInput from 'bootstrap-vue/es/components/form-input/form-input'
+import bButton from 'bootstrap-vue/es/components/button/button'
+
+export default {
+    name: 'SearchBar',
+    components: {
+        bNavForm,
+        bFormInput,
+        bButton
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/components/TopNav.vue
@@ -0,0 +1,26 @@
+<template>
+    <b-navbar type="dark" variant="primary">
+        <b-navbar-brand>Vue Bookstore</b-navbar-brand>
+        <SearchBar />
+    </b-navbar>
+</template>
+
+<script>
+import bNavbar from 'bootstrap-vue/es/components/navbar/navbar'
+import bNavbarBrand from 'bootstrap-vue/es/components/navbar/navbar-brand'
+import SearchBar from './SearchBar'
+// What is a plug-in? When Do I use one?
+
+export default {
+    name: 'TopNav',
+    components: {
+        bNavbar,
+        bNavbarBrand,
+        SearchBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
